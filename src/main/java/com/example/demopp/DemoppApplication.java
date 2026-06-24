4ffd4ba6b46d0a9072e78b464a77909d1dc5db7b
gecob2k26 <gecob2k26@gmail.com>
2026-06-24 21:32:22 +0800

    Initial BE

---
+++ src/main/java/com/example/demopp/DemoppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demopp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demopp/DemoppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demopp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
